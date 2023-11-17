f139c465357ba32e88a4b63aaca8be963be6284e
b11361213 <114211720+b11361213@users.noreply.github.com>
2023-11-17 18:20:32 +0800

    1117 - improve env install scripts, convert cmd command to PowerShell command

--- others/config.ps1
+++ others/config.ps1
@@ -1,24 +1,22 @@
 # 移動 xampp, htdocs DIR
-mv 'C:\Users\Administrator\Downloads\xampp\' 'C:\xampp\'
-rm -r 'C:\xampp\htdocs\'
+Move-Item 'C:\Users\Administrator\Downloads\xampp\' 'C:\xampp\'
+Remove-Item -Recurse 'C:\xampp\htdocs\*'
 xcopy 'C:\Users\Administrator\Downloads\Web-Page-Design-main\htdocs\*' 'C:\xampp\htdocs\' /e
 
 # 安裝 certificate
-mkdir 'C:\xampp\apache\crt\'
+New-Item -Path 'C:\xampp\apache\' -Name 'crt' -ItemType 'Directory'
 xcopy 'C:\Users\Administrator\Downloads\Web-Page-Design-main\others\cert.conf' 'C:\xampp\apache\crt\'
 xcopy 'C:\Users\Administrator\Downloads\Web-Page-Design-main\others\make-cert.bat' 'C:\xampp\apache\crt\'
 
-# explorer 'C:\xampp\apache\crt\'
-# & 'C:\xampp\apache\crt\make-cert.bat'
+Set-Location 'C:\xampp\apache\crt\'
 Start-Process 'C:\xampp\apache\crt\make-cert.bat'
 do { $q1 = Read-Host "執行 xampp\apache\crt\make-cert.bat 了嗎? (y/n)" } while ($q1 -ne 'y')
+Invoke-Item 'C:\xampp\apache\crt\'
 Write-Host "`n憑證安裝：`n`t  exec server.crt`n`t  安裝憑證`n`t  本機電腦`n`t  放入以下的存放區`n`t  受信任的授權單位`n`t  完成`n"
 do { $q1 = Read-Host "執行 crt\localhost\server.crt 安裝憑證了嗎? (y/n)" } while ($q1 -ne 'y')
 
-rm 'C:\xampp\apache\conf\extra\httpd-xampp.conf'
+Remove-Item 'C:\xampp\apache\conf\extra\httpd-xampp.conf'
 xcopy 'C:\Users\Administrator\Downloads\Web-Page-Design-main\others\httpd-xampp.conf' 'C:\xampp\apache\conf\extra\'
 
-# start apache and mysql
-do { $q1 = Read-Host "`n`t即將啟動 apache 與 mysql，是否啟動? (y/n)" } while ($q1 -ne 'y')
-Start-Process 'C:\xampp\apache_start.bat'
-Start-Process 'C:\xampp\mysql_start.bat'
+# 啟動 XAMPP
+Start-Process 'C:\xampp\xampp-control.exe'
